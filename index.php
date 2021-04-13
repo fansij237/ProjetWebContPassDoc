6ea7bf18098ec51d59e6dd52eca4012524636e64
Fansi Merimee <fansimerimee@gmail.com>
2021-04-13 03:33:55 -0500

    school project done

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
+
+This is just a school project we've been ask to do  
 -->
 <html>
     <head>
